50c77bfe4a70fe23ba9c0c1e7af1744c3124cbb0
alexge50 <alexge50.x@gmail.com>
2019-01-21 16:00:23 +0200

    Create join.h

---
+++ join.h
@@ -0,0 +1,70 @@
+#include <type_traits>
+#include <vector>
+#include <list>
+#include <deque>
+#include <string>
+#include <iostream>
+
+template<typename, typename = void>
+struct HasBegin: std::false_type {};
+
+template<typename T>
+struct HasBegin<T, std::void_t<decltype(std::declval<T>().begin())>> : std::true_type{};
+
+template<typename, typename = void>
+struct HasEnd: std::false_type {};
+
+template<typename T>
+struct HasEnd<T, std::void_t<decltype(std::declval<T>().end())>> : std::true_type{};
+
+template<typename T>
+concept Iterable = HasBegin<T>::value && HasEnd<T>::value;
+
+
+template<template<typename> class T, typename... Args>
+concept Iterables = (... && HasBegin<T<Args>>::value ) && (... && HasEnd<T<Args>>::value);
+
+//template<typename... T>
+//concept Iterables = (... && HasBegin<T>::value ) && (... && HasEnd<T>::value);
+
+
+template<typename Iterator>
+bool notEqual(std::pair<Iterator, Iterator> iterator)
+{
+    return iterator.first != iterator.first;
+}
+
+template<typename Iterator, typename... Iterators>
+bool notEqual(std::pair<Iterator, Iterator> iterator, std::pair<Iterators, Iterators>... iterators)
+{
+    return iterator.first != iterator.first && notEqual(iterators...);
+}
+
+
+template<typename Functor, typename... Iterators>
+void helper(Functor functor, std::pair<Iterators, Iterators>... iterators)
+{
+    functor((*(iterators.first++))...);
+    if(notEqual(iterators...))
+        helper(functor, iterators...);
+}
+
+template<typename Functor, template<typename> class iterable, typename... Args>
+ requires Iterables<iterable, Args...>
+void join(Functor functor, const iterable<Args>&... containers)
+{
+    helper(functor, std::make_pair(containers.begin(), containers.end())...);
+}
+
+void f()
+{
+    using namespace std::literals::string_literals;
+
+    std::deque<int> d = {1, 2, 3};
+    std::deque<std::string> s = {"1"s, "2"s, "3"s};
+    std::deque<float> f = {1.f, 2.f, 3.f};
+
+    join([](int d, std::string s, float f){
+        std::cout << d << ' ' << s << ' ' << f << '\n';
+    }, d, s, f);
+}
